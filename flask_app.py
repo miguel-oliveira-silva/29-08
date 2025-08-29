5990015906300ed9cb8626fbe13e7c6dbc03393e
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-08-28 23:07:19 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,24 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! Bem vindos alunos do IFSP'
+    return render_template('index.html', current_time = datetime.utcnow())
 
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', nome=name)
+
+@app.route('/user/')
+def userr():
+    return render_template('user.html')
+
+@app.route('/rotainexistente')
+def rotainexistente():
+    return render_template('404.html')
